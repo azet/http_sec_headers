1252aae25a379028cb3e66237d01b4c1c9ff2370
Aaron Zauner <azet@azet.org>
2014-10-02 22:05:31 +0200

    add more http security headers

--- README.md
+++ README.md
@@ -3,12 +3,19 @@ Scripts to check for HTTP Security Headers.
 
 ### Usage
 
-  * `headers.rb`:
+#### headers.rb
 ```
-# ruby headers.rb http://bettercrypto.org
-[+] http://bettercrypto.org redirects to HTTPS.
-[+] https://bettercrypto.org supports HSTS.
+$ ruby headers.rb http://twitter.com
+[+] http://twitter.com redirects to HTTPS
+[+] https://twitter.com sets Content-Security-Policy
+[+] https://twitter.com supports HSTS
+[+] https://twitter.com set X-Content-Type-Options to nosniff
+[+] https://twitter.com set X-Frame-Options to SAMEORIGIN
+[+] https://twitter.com set XSS Protection (X-Xss-Protection: 1; mode=block)
 ```
+
+### Resources
+  * [OWASP Wiki on Security Related HTTP Headers](https://www.owasp.org/index.php/List_of_useful_HTTP_headers)
 
 ## License
 CC0 1.0: https://creativecommons.org/publicdomain/zero/1.0/
--- headers.rb
+++ headers.rb
@@ -12,8 +12,20 @@ end
 page = ARGV.first
 begin
   open(page).meta.each do |x|
-    puts "[+] #{page} supports HSTS." if x.first == "strict-transport-security"
-    puts "[+] #{page} supports HPKP." if x.first == "public-key-pins"
+    x.map!(&:downcase)
+    puts "[+] #{page} supports HSTS" if x.first == "strict-transport-security"
+    puts "[+] #{page} supports HPKP" if x.first == "public-key-pins"
+    puts "[+] #{page} provides 'Clickjacking Protection' (X-Frame-Options: deny)" \
+      if x == ['x-frame-options', 'deny']
+    puts "[+] #{page} set X-Frame-Options to SAMEORIGIN"                          \
+      if x == ['x-frame-options', 'sameorigin']
+    puts "[+] #{page} set X-Content-Type-Options to nosniff"                      \
+      if x == ['x-content-type-options', 'nosniff']
+    puts "[+] #{page} set XSS Protection (X-Xss-Protection: 1; mode=block)"       \
+      if x == ['x-xss-protection', '1; mode=block']
+    puts "[+] #{page} sets Content-Security-Policy"                               \
+      if x.first == "content-security-policy" or                                  \
+         x.first == "content-security-policy-report-only"
   end
 rescue => e
   puts "[+] #{page} redirects to HTTPS." if e.message =~ /redirection/
